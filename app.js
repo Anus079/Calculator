88a26acd8fd067bce5a7e659b9d8d0dea2489a80
Anus079 <anuszahid0798@gmail.com>
2020-06-27 20:54:33 +0500

    Simple Calculator

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ function clearresult(){
     
 }
 function open(){
-   
+    var veiw = document.getElementById("view")
+    veiw.value = "0";
 }
 
 function getresult(){
     var veiw = document.getElementById("view")
     veiw.value = eval(veiw.value)
+}
+
+function clearone() {
+    var getnumber = document.getElementById("view");
+    var str = view.value;
+    var num = str.length;
+    var str1 = str.slice(0, num - 1);
+    view.value = str1;
+    console.log(str1)
 }
